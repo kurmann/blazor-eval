1fdb80a198bb16a1c6a18e1c73a19554e338a92e
Patrick Kurmann <kurmannwillisau@me.com>
2018-07-25 15:51:29 +0200

    Created empty todo Blazor page including model

---
+++ BlazorEvalApp/Pages/Todo.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace BlazorEvalApp.Pages
+{
+    public class TodoModel : BlazorComponent
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
