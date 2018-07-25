59e05a9a04dea6a7e5bc4262a48cdbf0898b4c50
Patrick Kurmann <kurmannwillisau@me.com>
2018-07-25 16:25:55 +0200

    Defined a SimpleList model

---
+++ BlazorComponents/SimpleList.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Blazor.Components;
+using System.Collections.Generic;
+
+namespace BlazorComponents
+{
+    public class SimpleListModel : BlazorComponent
+    {
+        public List<string> listItems = new List<string>();
+        public string newItem;
+
+        public void AddItem()
+        {
+            if (string.IsNullOrEmpty(newItem))
+                return;
+
+            listItems.Add(newItem);
+            newItem = "";
+        }
+    }
+}
